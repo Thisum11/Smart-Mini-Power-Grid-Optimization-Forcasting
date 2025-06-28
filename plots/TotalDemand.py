e49b1c449fedc1e731391c1e61b49c5d2bc7c35c
Thisum11 <147669362+Thisum11@users.noreply.github.com>
2025-06-28 22:13:16 +0930

    Fixing the total demand plot

--- plots/TotalDemand.py
+++ plots/TotalDemand.py
@@ -1,4 +1,16 @@
+import pandas as pd
 import matplotlib.pyplot as plt
+
+#Loading the cleaned data
+df = pd.read_csv(r'D:\New folder\Smart-Mini-Power-Grid-Optimization-Forcasting\analysis\dataSet01cleaned.csv')
+
+df['date'] = pd.to_datetime(df['date'])
+
+# To calculate the total demand
+if 'total_demand' not in df.columns:
+    mw_columns = [col for col in df.columns if col.endswith('- MW')]
+    df['total_demand'] = df[mw_columns].sum(axis=1)
+
 
 plt.figure(figsize=(12, 6))
 plt.plot(df['date'], df['total_demand'], color='blue')
@@ -8,6 +20,6 @@ plt.ylabel('Total Demand (MW)')
 plt.xticks(rotation=45)
 plt.grid(True)
 plt.tight_layout()
-
+plt.savefig('total_demand.png')
 plt.show()
 
