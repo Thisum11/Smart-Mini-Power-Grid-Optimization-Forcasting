5d41d670796b00585d19a5b8571f8fbf7dca44a6
Thisum11 <147669362+Thisum11@users.noreply.github.com>
2025-06-30 01:13:53 +0930

    Updating on get_cost
    Creating the getter get_cost

--- src/classes/generator.py
+++ src/classes/generator.py
@@ -5,13 +5,19 @@ class Generator:
     __name: str
     __max_capacity: float
     __min_capacity: float
-    __cost_per_MWh: float
+    __cost: float
     __source_type: str
     __is_renewable: bool
     __current_output: float = field(default=0)
 
+    #Generates the power amount that generates by the generator
     def generate (self, amount: float):
         if (amount <= self.__max_capacity and amount >= self.__min_capacity):
            self.__current_output = amount
         else:
             raise ValueError(f"Requested amount {amount} is out of bounds.")
+
+    #Getter method for the cost
+    @property
+    def get_cost(self):
+        return self.__cost
