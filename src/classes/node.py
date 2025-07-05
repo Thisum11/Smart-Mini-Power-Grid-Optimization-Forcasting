362c9fc4b9c9f6066523e8e280f2f34d7a84e988
Thisum11 <147669362+Thisum11@users.noreply.github.com>
2025-07-06 01:15:10 +0930

    Update node.py
    Creating the rest of the methods

--- src/classes/node.py
+++ src/classes/node.py
@@ -5,10 +5,26 @@ class Node:
     nodeID : str
     name : str
     node_type : str
+    connections = []
 
     # validating the data class
     def __post_init__(self):
         valid_types = {"generator", "consumer", "junction"}
         if self.node_type.lower() not in valid_types:
             raise ValueError(f"Invalid node type: {self.node_type}")
+
+    # Returning the information
+    def get_info(self):
+        return {
+            "ID"   : self.nodeID,
+            "Name" : self.name,
+            "Type" : self.node_type,
+        }
+
+    # allowing nodes to create
+    def connection(self, other):
+        self.connections.append(other)
+
+    def get_connections(self):
+        return self.connections
 
