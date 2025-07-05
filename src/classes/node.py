1beca3618a9725aaf2f5a75f00d7654c4293a464
Thisum11 <147669362+Thisum11@users.noreply.github.com>
2025-07-06 00:55:59 +0930

    Update node.py
    Creating the __post_innit method

--- src/classes/node.py
+++ src/classes/node.py
@@ -5,3 +5,10 @@ class Node:
     nodeID : str
     name : str
     node_type : str
+
+    # validating the data class
+    def __post_init__(self):
+        valid_types = {"generator", "consumer", "junction"}
+        if self.node_type.lower() not in valid_types:
+            raise ValueError(f"Invalid node type: {self.node_type}")
+
